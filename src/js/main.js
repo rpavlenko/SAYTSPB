78c4bc0670e2faab53792b107686410fd830dd85
DESKTOP-ULQFVU9\Sims <rpavlenko.mail@gmail.com>
2020-04-04 02:44:34 +0200

    feat&fix: add scrolling to menu & padding/margin fixes to section, titles etc

--- src/js/main.js
+++ src/js/main.js
@@ -246,4 +246,13 @@ $(document).ready(function () {
       }
     }
 
+    // anchor scrolls
+      $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+          scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+      });
+
 });
